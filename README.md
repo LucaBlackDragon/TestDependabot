a95d52390d584e61816f294eba8c590bf7c86dc9
Luca Nardi <l.nardi@pluservice.net>
2025-07-30 14:47:03 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Dependabot
+
+This is a test repository for Dependabot. It contains a simple console application that parses JSON files. A vunerable version of the `System.Text.Json` package is used to demonstrate how Dependabot can help keep dependencies up to date.
---
+++ TestDependabot/Program.cs
@@ -0,0 +1,35 @@
+﻿// print current directory
+Console.WriteLine($"Current Directory: {Environment.CurrentDirectory}");
+string? path = Environment.GetCommandLineArgs().Length > 1 ? Environment.GetCommandLineArgs()[1] : null;
+if (string.IsNullOrEmpty(path))
+{
+    Console.WriteLine("Insert path to JSON file to be parsed:");
+    path = Console.ReadLine();
+}
+if (string.IsNullOrEmpty(path))
+{
+    Console.WriteLine("No path provided. Exiting.");
+    return;
+}
+Console.WriteLine($"Parsing JSON file at: {path}");
+if (!File.Exists(path))
+{
+    Console.WriteLine($"File not found: {path}");
+    return;
+}
+try
+{
+    string jsonContent = await File.ReadAllTextAsync(path);
+    System.Text.Json.JsonSerializer.Deserialize<object>(jsonContent);
+    Console.WriteLine("JSON file parsed successfully.");
+}
+catch (System.Text.Json.JsonException ex)
+{
+    Console.WriteLine($"Error parsing JSON file: {ex.Message}");
+    return;
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+    return;
+}
